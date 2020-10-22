b1d1559c18fa600bd4b55036f9a5757a62840f62
Michael <363966403@qq.com>
2020-10-22 13:37:49 +0800

    1161

---
+++ src/main/java/michael/slf4j/learning/hdu/C1161_EddyMistakes.java
@@ -0,0 +1,15 @@
+package michael.slf4j.learning.hdu;
+
+import java.util.Scanner;
+
+public class C1161_EddyMistakes {
+
+	public static void main(String[] args) {
+		try(Scanner sc = new Scanner(System.in)){
+			while(sc.hasNext()) {
+				System.out.println(sc.nextLine().toLowerCase());
+			}
+		}
+	}
+
+}
