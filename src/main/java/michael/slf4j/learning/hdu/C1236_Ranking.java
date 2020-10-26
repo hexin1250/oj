306ed997bfac3b9cc785ed5c2c5916cb70ad08bb
Michael <363966403@qq.com>
2020-10-26 16:35:45 +0800

    1236

---
+++ src/main/java/michael/slf4j/learning/hdu/C1236_Ranking.java
@@ -0,0 +1,79 @@
+package michael.slf4j.learning.hdu;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class C1236_Ranking {
+
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			while(sc.hasNext()) {
+				int n = sc.nextInt();
+				if(n == 0) {
+					break;
+				}
+				int m = sc.nextInt();
+				int baseLine = sc.nextInt();
+				int[] ques = new int[m];
+				for (int i = 0; i < m; i++) {
+					ques[i] = sc.nextInt();
+				}
+				sc.nextLine();
+				List<StudentResult> list = new ArrayList<>();
+				for (int i = 0; i < n; i++) {
+					String line = sc.nextLine();
+					String[] parts = line.split("[\\s]+");
+					/**
+					 * 考虑是否存在字符串字典序和数字字典序的问题。
+					 */
+					int myQues = Integer.valueOf(parts[1]);
+					int sum = 0;
+					for (int j = 0; j < myQues; j++) {
+						int currentQuesId = Integer.valueOf(parts[2 + j]);
+						sum += ques[currentQuesId - 1];
+					}
+					if(sum >= baseLine) {
+						list.add(new StudentResult(parts[0], sum));
+					}
+				}
+				Collections.sort(list, new Comparator<StudentResult>() {
+					@Override
+					public int compare(StudentResult o1, StudentResult o2) {
+						if(o1.getScore() > o2.getScore()) {
+							return -1;
+						} else if(o1.getScore() < o2.getScore()) {
+							return 1;
+						}
+						return o1.getId().compareTo(o2.getId());
+					}
+				});
+				System.out.println(list.size());
+				list.forEach(r -> System.out.println(r));
+			}
+		}
+	}
+	
+	public static class StudentResult {
+		private String id;
+		private int score = 0;
+		public StudentResult(String id, int score) {
+			this.id = id;
+			this.score = score;
+		}
+		public String getId() {
+			return id;
+		}
+		public int getScore() {
+			return score;
+		}
+		public String toString() {
+			StringBuffer sb = new StringBuffer();
+			sb.append(id).append(' ').append(score);
+			return sb.toString();
+		}
+	}
+
+}
