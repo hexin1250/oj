49865ad35a5430fd42baccff9b10e811b7cd64b4
hexin1250 <363966403@qq.com>
2021-01-01 19:05:16 +0800

    add comment.

--- src/main/java/michael/slf4j/learning/hdu/C1409_IsItNumber.java
+++ src/main/java/michael/slf4j/learning/hdu/C1409_IsItNumber.java
@@ -10,7 +10,14 @@ public class C1409_IsItNumber {
 	private static List<String> list = new ArrayList<>();
 	
 	static{
+		/**
+		 * 解决大部分数字问题，除了下面这个例子。
+		 * .123E123
+		 */
 		list.add("^[\\d]+([.][\\d]*)?([E][+-]?[\\d]+)?$");
+		/**
+		 * 补齐上面缺失的部分。
+		 */
 		list.add("^[.][0-9]*(E[-|+]?[0-9]+)?$");
 	}
 
