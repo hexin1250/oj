aa6353139a53badcc5b94687a11f1726e47b9b9c
Michael <363966403@qq.com>
2021-01-04 14:55:51 +0800

    1415 format

--- src/main/java/michael/slf4j/learning/hdu/C1415_Jugs.java
+++ src/main/java/michael/slf4j/learning/hdu/C1415_Jugs.java
@@ -19,36 +19,39 @@ public class C1415_Jugs {
 			}
 		}
 	}
-	
+
 	public static class FillWater {
 		private final int a;
 		private final int b;
 		private Queue<Status> q;
 		private Set<Status> mem;
-		public FillWater(int a, int b){
+
+		public FillWater(int a, int b) {
 			this.a = a;
 			this.b = b;
 			q = new LinkedBlockingQueue<>();
 			mem = new HashSet<>();
 		}
-		public void bfs(int c){
+
+		public void bfs(int c) {
 			Status init = new Status(null, 0, 0, null);
 			q.add(init);
 			mem.add(init);
 			Status end = null;
-			while(!q.isEmpty()){
+			while (!q.isEmpty()) {
 				Status s = q.poll();
-				if(s.getB() == c){
+				if (s.getB() == c) {
 					end = s;
 					break;
 				}
 				expand(s);
 			}
-			if(end != null){
+			if (end != null) {
 				System.out.println(end.getPath());
 				System.out.println("success");
 			}
 		}
+
 		private void expand(Status s) {
 			int a = s.getA();
 			int b = s.getB();
@@ -58,10 +61,10 @@ public class C1415_Jugs {
 				Status next = null;
 				String path = null;
 				boolean skip = false;
-				switch(i){
+				switch (i) {
 				case 0:
 					path = "fill A";
-					if(nextA != 0){
+					if (nextA != 0) {
 						skip = true;
 					} else {
 						nextA = this.a;
@@ -69,7 +72,7 @@ public class C1415_Jugs {
 					break;
 				case 1:
 					path = "fill B";
-					if(nextB != 0){
+					if (nextB != 0) {
 						skip = true;
 					} else {
 						nextB = this.b;
@@ -77,7 +80,7 @@ public class C1415_Jugs {
 					break;
 				case 2:
 					path = "empty A";
-					if(nextA != this.a){
+					if (nextA != this.a) {
 						skip = true;
 					} else {
 						nextA = 0;
@@ -85,7 +88,7 @@ public class C1415_Jugs {
 					break;
 				case 3:
 					path = "empty B";
-					if(nextB != this.b){
+					if (nextB != this.b) {
 						skip = true;
 					} else {
 						nextB = 0;
@@ -93,11 +96,11 @@ public class C1415_Jugs {
 					break;
 				case 4:
 					path = "pour A B";
-					if(nextB == this.b || nextA == 0){
+					if (nextB == this.b || nextA == 0) {
 						skip = true;
 					} else {
 						nextB += nextA;
-						if(nextB >= this.b){
+						if (nextB >= this.b) {
 							nextA = nextB - this.b;
 							nextB = this.b;
 						} else {
@@ -105,26 +108,26 @@ public class C1415_Jugs {
 						}
 					}
 					break;
-					default:
-						path = "pour B A";
-						if(nextA == this.a || nextB == 0){
-							skip = true;
+				default:
+					path = "pour B A";
+					if (nextA == this.a || nextB == 0) {
+						skip = true;
+					} else {
+						nextA += nextB;
+						if (nextA >= this.a) {
+							nextB = nextA - this.a;
+							nextA = this.a;
 						} else {
-							nextA += nextB;
-							if(nextA >= this.a){
-								nextB = nextA - this.a;
-								nextA = this.a;
-							} else {
-								nextB = 0;
-							}
+							nextB = 0;
 						}
-						break;
+					}
+					break;
 				}
-				if(skip){
+				if (skip) {
 					continue;
 				}
 				next = new Status(s, nextA, nextB, path);
-				if(mem.contains(next)){
+				if (mem.contains(next)) {
 					continue;
 				}
 				mem.add(next);
@@ -132,45 +135,52 @@ public class C1415_Jugs {
 			}
 		}
 	}
-	
+
 	public static class Status {
 		private final Status pre;
 		private final int a;
 		private final int b;
 		private final String path;
-		public Status(Status pre, int a, int b, String path){
+
+		public Status(Status pre, int a, int b, String path) {
 			this.pre = pre;
 			this.a = a;
 			this.b = b;
 			this.path = path;
 		}
+
 		public int getA() {
 			return a;
 		}
+
 		public int getB() {
 			return b;
 		}
-		public String getPath(){
+
+		public String getPath() {
 			StringBuffer sb = new StringBuffer();
-			if(pre != null){
-				if(!pre.getPath().isEmpty()){
+			if (pre != null) {
+				if (!pre.getPath().isEmpty()) {
 					sb.append(pre.getPath()).append("\r\n");
 				}
 				sb.append(path);
 			}
 			return sb.toString();
 		}
+
 		@Override
 		public boolean equals(Object obj) {
-			if(obj == null || !(obj instanceof Status)){
+			if (obj == null || !(obj instanceof Status)) {
 				return false;
 			}
 			return hashCode() == obj.hashCode();
 		}
+
 		@Override
 		public int hashCode() {
 			return a + 1000 * b;
 		}
+
 		@Override
 		public String toString() {
 			StringBuffer sb = new StringBuffer();
