6fd2d75c14e8d261b3d46497bf19546f01ba8350
HP <HP@Michael>
2025-08-25 00:46:57 +0800

    add 39

---
+++ src/main/java/michael/slf4j/learning/leetcode/C39.java
@@ -0,0 +1,37 @@
+package michael.slf4j.learning.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class C39 {
+
+	public static void main(String[] args) {
+	}
+	
+	public List<List<Integer>> combinationSum(int[] candidates, int target) {
+		Arrays.sort(candidates);
+		return traversal(candidates, target, 0, new ArrayList<>());
+	}
+
+	private List<List<Integer>> traversal(int[] candidates, int target, int index, List<Integer> queue) {
+		List<List<Integer>> ret = new ArrayList<>();
+		if(target == 0) {
+			List<Integer> currentRet = new ArrayList<>(queue);
+			ret.add(currentRet);
+			return ret;
+		}
+		int size = queue.size();
+		for (int i = index; i < candidates.length; i++) {
+			int num = candidates[i];
+			if(num > target) {
+				break;
+			}
+			queue.add(num);
+			ret.addAll(traversal(candidates, target - num, i, queue));
+			queue.remove(size);
+		}
+		return ret;
+	}
+
+}
