27002cc70fc54f8f73c9e892ee79af6ea288551e
HP <HP@Michael>
2025-09-04 22:55:18 +0800

    add

--- src/main/java/michael/slf4j/learning/leetcode/C420.java
+++ src/main/java/michael/slf4j/learning/leetcode/C420.java
@@ -10,91 +10,98 @@ public class C420 {
 
 	public static void main(String[] args) {
 		C420 c = new C420();
-		String s = "Aaaaa";
+		String s = "aaaABCD5555ABCD134kkkkk";
 		System.out.println(c.strongPasswordChecker(s));
 	}
 	
 	private int upper = 0;
 	private int lower = 0;
 	private int number = 0;
-	private int duplicatedNum = 0;
-	private Map<Integer, List<Integer>> map = new HashMap<>();
+	private int duplicatedSum = 0;
+	private Map<Integer, List<Integer>> duplicatedMap = new HashMap<>();
 
 	public int strongPasswordChecker(String password) {
-		map.put(0, new ArrayList<>());
-		map.put(1, new ArrayList<>());
-		map.put(2, new ArrayList<>());
+		duplicatedMap.put(0, new ArrayList<>());
+		duplicatedMap.put(1, new ArrayList<>());
+		duplicatedMap.put(2, new ArrayList<>());
 		int len = password.length();
 		char pre = password.charAt(0);
 		detectChar(pre);
-		int duplicated = 1;
+		int duplicatedCount = 1;
 		for (int i = 1; i < len; i++) {
 			char c = password.charAt(i);
 			detectChar(c);
 			if(c == pre) {
-				duplicated++;
+				duplicatedCount++;
 			} else {
-				if(duplicated >= 3) {
-					int currentNum = duplicatedChangeCount(duplicated);
-					duplicatedNum += currentNum;
-					int mod = duplicated % 3;
-					List<Integer> list = map.get(mod);
-					list.add(currentNum);
+				if(duplicatedCount >= 3) {
+					int replaceNum = duplicatedReplaceCount(duplicatedCount);
+					duplicatedSum += replaceNum;
+					int mod = duplicatedCount % 3;
+					List<Integer> duplicatedArr = duplicatedMap.get(mod);
+					duplicatedArr.add(replaceNum);
 				}
-				duplicated = 1;
+				duplicatedCount = 1;
 				pre = c;
 			}
 		}
-		if(duplicated >= 3) {
-			int currentNum = duplicatedChangeCount(duplicated);
-			duplicatedNum += currentNum;
-			int mod = duplicated % 3;
-			List<Integer> list = map.get(mod);
-			list.add(currentNum);
+		if(duplicatedCount >= 3) {
+			int replaceNum = duplicatedReplaceCount(duplicatedCount);
+			duplicatedSum += replaceNum;
+			int mod = duplicatedCount % 3;
+			List<Integer> duplicatedArr = duplicatedMap.get(mod);
+			duplicatedArr.add(replaceNum);
 		}
-		int error = 3 - (upper + lower + number);
+		int cap = upper + lower + number;
 		if(len < 6) {
-			return Math.max(6 - len, error);
+			return Math.max(6 - len, 3 - cap);
 		} else if(len <= 20) {
-			return Math.max(duplicatedNum, error);
+			return Math.max(duplicatedSum, 3 - cap);
 		} else {
-			int target = len - 20;
-			int times = target;
-			List<Integer> list0 = map.get(0);
-			List<Integer> list1 = map.get(1);
-			List<Integer> list2 = map.get(2);
-			int sum = 0;
+			int targetDeleteCharCount = len - 20;
+			int ret = targetDeleteCharCount;
+			List<Integer> duplicatedArr0 = duplicatedMap.get(0);
+			List<Integer> duplicatedArr1 = duplicatedMap.get(1);
+			List<Integer> duplicatedArr2 = duplicatedMap.get(2);
+			int updatedReplaceSum = 0;
 			
-			Iterator<Integer> it = list0.iterator();
+			/**
+			 * 优先对余数为0的做删除字符操作，删除一个字符，减少一次操作
+			 */
+			Iterator<Integer> it = duplicatedArr0.iterator();
 			while(it.hasNext()) {
-				int num = it.next();
-				
-				if(target > 0) {
-					num -= 1;
-					target--;
+				int replaceNum = it.next();
+				if(targetDeleteCharCount > 0) {
+					replaceNum -= 1;
+					targetDeleteCharCount--;
 				}
-				sum += num;
+				updatedReplaceSum += replaceNum;
 			}
 			
-			Iterator<Integer> it1 = list1.iterator();
+			/**
+			 * 对余数为1的做删除字符操作，删除两个字符，减少一次操作
+			 */
+			Iterator<Integer> it1 = duplicatedArr1.iterator();
 			while(it1.hasNext()) {
-				int num = it1.next();
-				
-				if(target > 1) {
-					num -= 1;
-					target -= 2;
+				int replaceNum = it1.next();
+				if(targetDeleteCharCount > 1) {
+					replaceNum -= 1;
+					targetDeleteCharCount -= 2;
 				}
-				sum += num;
+				updatedReplaceSum += replaceNum;
 			}
 			
-			for (Integer i : list2) {
-				sum += i;
+			for (Integer i : duplicatedArr2) {
+				updatedReplaceSum += i;
 			}
-			int mod = target % 3;
-			int count = (target - mod) / 3;
-			sum -= count;
-			times += Math.max(sum, error);
-			return times;
+			int mod = targetDeleteCharCount % 3;
+			/**
+			 * 对余数为2的做删除字符操作，删除三个字符，减少一次操作
+			 */
+			int count = (targetDeleteCharCount - mod) / 3;
+			updatedReplaceSum -= count;
+			ret += Math.max(updatedReplaceSum, 3 - cap);
+			return ret;
 		}
 	}
 	
@@ -108,7 +115,7 @@ public class C420 {
 		}
 	}
 	
-	private int duplicatedChangeCount(int number) {
+	private int duplicatedReplaceCount(int number) {
 		int mod = number % 3;
 		return (number - mod) / 3;
 	}
---
+++ src/main/java/michael/slf4j/learning/leetcode/C429.java
@@ -0,0 +1,85 @@
+package michael.slf4j.learning.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class C429 {
+
+	public static void main(String[] args) {
+	}
+
+	public List<List<Integer>> levelOrder(Node root) {
+		List<List<Integer>> ret = new ArrayList<>();
+		dfs(ret, 0, root);
+		return ret;
+	}
+	
+	private void dfs(List<List<Integer>> ret, int level, Node node) {
+		if(node == null) {
+			return;
+		}
+		int size = ret.size();
+		if(size - 1 < level) {
+			ret.add(new ArrayList<>());
+		}
+		ret.get(level).add(node.val);
+		if(node.children != null) {
+			int len = node.children.size();
+			for (int i = 0; i < len; i++) {
+				Node child = node.children.get(i);
+				dfs(ret, level + 1, child);
+			}
+		}
+	}
+
+	public List<List<Integer>> levelOrder1(Node root) {
+		if(root == null) {
+			return new ArrayList<List<Integer>>();
+		}
+		Queue<Node> q = new LinkedBlockingQueue<>();
+		q.add(root);
+		int count = 1;
+		int nextCount = 0;
+		int index = 0;
+		List<List<Integer>> ret = new ArrayList<>();
+		while(!q.isEmpty()) {
+			if(index == 0) {
+				ret.add(new ArrayList<>());
+			}
+			index++;
+			Node node = q.poll();
+			int size = ret.size();
+			if(node.children != null) {
+				ret.get(size - 1).add(node.val);
+				q.addAll(node.children);
+				nextCount += node.children.size();
+			}
+			if(index == count) {
+				index = 0;
+				count = nextCount;
+				nextCount = 0;
+			}
+		}
+		return ret;
+	}
+
+	public static class Node {
+		public int val;
+		public List<Node> children;
+
+		public Node() {
+		}
+
+		public Node(int _val) {
+			val = _val;
+		}
+
+		public Node(int _val, List<Node> _children) {
+			val = _val;
+			children = _children;
+		}
+	};
+
+}
